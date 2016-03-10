7b849bc936c0f00c00b41d53b9fb06d33aa2a335
Catalin Trifu <ctrifu@gmail.com>
2016-03-10 12:25:27 +0200

    auto-commit

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,18 +1,47 @@
 class loopback::install () {
-  file {'/etc/systemd/scripts':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0775',
+  file {
+    '/etc/systemd/scripts':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0775';
+    '/etc/udev/rules.d':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0775';
+    '/etc/udev/scripts':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0775';
   }
 
-  file { 'loopback.service':
-    ensure => present,
-    path   => '/etc/systemd/system/loopback.service',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0664',
-    source => "puppet:///modules/${module_name}/loopback.service",
+  file {
+    'loopback.service':
+      ensure     => present,
+      path       => '/etc/systemd/system/loopback.service',
+      owner      => 'root',
+      group      => 'root',
+      mode       => '0664',
+      source     => "puppet:///modules/${module_name}/loopback.service",
+      require    => File['/etc/systemd/scripts'];
+    'loop-setup':
+      ensure  => present,
+      path    => '/etc/udev/scripts/loop-setup',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '775',
+      source  => "puppet:///modules/${module_name}/udev/scripts/loop-setup",
+      require => File['/etc/udev/scripts'];
+    '50-loop':
+      ensure  => present,
+      path    => '/etc/udev/rules.d/50-loop.rules',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '644',
+      source  => "puppet:///modules/${module_name}/udev/rules.d/50-loop.rules",
+      require => File['/etc/udev/rules.d'];
   }
 
   exec { 'systemd-daemon-reload':
